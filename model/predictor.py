c16cdfc9d88e3d5523b486fd7eeef9bd1663621e
Mayank Sharma <mayank.sharma@aptusdatalabs.com>
2020-07-03 13:48:40 +0530

    testv1

--- model/predictor.py
+++ model/predictor.py
@@ -1,36 +1,11 @@
 import os
-import pickle
+import joblib
 import io
 import flask
 import pandas as pd
 
+
 model_path = os.environ["MODEL_PATH"]
-
-
-# A singleton for holding the model. This simply loads the model and holds it.
-# It has a predict function that does a prediction based on the model and the input data.
-
-
-class ScoringService(object):
-    model = None  # Where we keep the model when it's loaded
-
-    @classmethod
-    def get_model(cls):
-        """Get the model object for this instance, loading it if it's not already loaded."""
-        if cls.model is None:
-            with open(os.path.join(model_path, "model.pkl"), "rb") as f:
-                cls.model = pickle.load(f)
-        return cls.model
-
-    @classmethod
-    def predict(cls, x):
-        """For the input, do the predictions and return them.
-
-        Args:
-            input (a pandas dataframe): The data on which to do the predictions. There will be
-                one prediction per row in the dataframe"""
-        clf = cls.get_model()
-        return clf.predict(x)
 
 
 # The flask app for serving predictions
@@ -56,24 +31,28 @@ def transformation():
     just means one prediction per line, since there's a single column.
     """
     data = None
+    print(request.form.to_dict())
+    
+    with open(os.path.join(model_path, "model.pkl"),'rb') as f:
+        model = joblib.load(f)
+        
+    data = request.form.to_dict()
+    
 
-    # Convert from CSV to pandas
-    if flask.request.content_type == "text/csv":
-        data = flask.request.data.decode("utf-8")
-        s = io.StringIO(data)
-        data = pd.read_csv(s, header=None)
-    else:
-        return flask.Response(
-            response="This predictor only supports CSV data",
-            status=415,
-            mimetype="text/plain",
-        )
+    for i in data:
+        if i not in ['isholiday','Type']:
+            if i not in ['Store','Dept','Size','Month','Type_A','Type_B','Type_C']:
+                data[i] = float(data[i])
+            else:
+                data[i] = int(data[i])
+        data[i] = [data[i]]
+    
 
-    print("Invoked with {} records".format(data.shape[0]))
+    test = pd.DataFrame(data)
+    test.isholiday = test.isholiday == 'True'
+    pred = model.predict(test)
 
-    # Do the prediction
-    predictions = ScoringService.predict(data)
-
+    print(pred)
     # Convert from numpy back to CSV
     out = io.StringIO()
     pd.DataFrame({"results": predictions}).to_csv(out, header=False, index=False)
